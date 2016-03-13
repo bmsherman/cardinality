0fc5744d6e5d3573e004a925e1ee6fd0ee85b642
Tej Chajed <tchajed@gmail.com>
2016-03-12 19:40:15 -0500

    Fix references to Coq terms in coqdoc comments

--- finite_function.v
+++ finite_function.v
@@ -1,10 +1,10 @@
 (**
 
-Isomorphism between functions on Fin.t n and Vector.t A n.
+Isomorphism between functions on [Fin.t n] and [Vector.t A n].
 
 Defines a function to turn a finite function into a vector of its
 values. Proves that this function is the inverse (both left and right)
-of Vector.nth, giving an isomorphism.
+of [Vector.nth], giving an isomorphism.
 
 *)
 
@@ -18,6 +18,8 @@ Set Implicit Arguments.
 Definition fin_f_Sn A n (f: Fin.t (S n) -> A) : Fin.t n -> A :=
   (fun x => f (Fin.FS x)).
 
+(* The main definition: converts a finite function into a vector of
+its values. *)
 Definition fin_to_vec A n (f: Fin.t n -> A) : Vector.t A n.
 Proof.
   induction n.
@@ -32,10 +34,11 @@ Proof.
   apply (fin_f_Sn f).
 Defined.
 
+(** The inverse of [fin_to_vec] is already defined as [Vector.nth]. *)
 Definition vec_to_fin A n (v: Vector.t A n) : Fin.t n -> A :=
   (fun x => Vector.nth v x).
 
-(* One direction of the main fin_to_vec correctness proof. *)
+(* One direction of the main [fin_to_vec] correctness proof. *)
 Theorem fin_to_vec_inverse : forall A n (f: Fin.t n -> A),
   forall x, vec_to_fin (fin_to_vec f) x = f x.
 Proof.
@@ -57,7 +60,7 @@ Defined.
 
 (** Reduction proof that converting a cons vector is the same as
 converting the vector and then adding a single mapping in the sense of
-fin_f_add. *)
+[fin_f_add]. *)
 Lemma fin_to_vec_cons : forall A n (v: Vector.t A n) a,
     vec_to_fin (Vector.cons _ a _ v) =
     fin_f_add (vec_to_fin v) a.
@@ -71,7 +74,7 @@ Qed.
 a vector is the same as cons'ing the element onto the original
 function.
 
-Whereas fin_to_vec actually uses (dependent) recursion over n, this
+Whereas [fin_to_vec] actually uses (dependent) recursion over n, this
 proves equivalence to a recursive definition on the structure of the
 vector. *)
 Lemma vec_fin_f_add : forall A n (f: Fin.t n -> A) a,
@@ -82,7 +85,7 @@ Proof.
   induction n; cbn; auto.
 Qed.
 
-(* The other direction of the fin_to_vec correctness proof. *)
+(* The other direction of the [fin_to_vec] correctness proof. *)
 Theorem vec_to_fin_inverse : forall A n (v: Vector.t A n),
     fin_to_vec (vec_to_fin v) = v.
 Proof.
@@ -97,7 +100,7 @@ Proof.
   auto.
 Qed.
 
-(** Package isomorphism functions and theorems in an Iso.T. *)
+(** Package isomorphism functions and theorems in an [Iso.T]. *)
 Definition fin_vec_iso A n : Iso.T (Fin.t n -> A) (Vector.t A n).
 Proof.
   refine ({| Iso.to := @fin_to_vec A n;
